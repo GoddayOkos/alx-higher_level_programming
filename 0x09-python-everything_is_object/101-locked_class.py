8e2093390e902ab3d6ae28dc91eb715aca46b3cc
GoddayOkos <goddayokos@gmail.com>
2022-10-19 14:41:12 -0700

    Mandatory answers

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 2022
+@author: Godday Okoduwa
+"""
+
+
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+Everything as Object -Python
