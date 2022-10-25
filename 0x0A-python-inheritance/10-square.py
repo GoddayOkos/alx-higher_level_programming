f6079c9b5825c701b7f750b6ee658a5bbfcdccb4
GoddayOkos <goddayokos@gmail.com>
2022-10-25 02:52:44 -0700

    Square is a Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -24,3 +24,12 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+        Return:
+            The area of the square
+        """
+        return self.__size ** 2
