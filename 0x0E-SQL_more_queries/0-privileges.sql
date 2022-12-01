903371fbf85287e0435637b85a8b10bcdbdeca6c
GoddayOkos <goddayokos@gmail.com>
2022-12-01 03:08:12 -0800

    Started mandatory tasks

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- List privileges using SHOW GRANTS
+-- DML QUERY to list all privileges for user_0d_1 and user_0d_2 in localhost
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
