367ed910fd1e171c3d8cbe2ef546e1566ce11bd3
GoddayOkos <goddayokos@gmail.com>
2022-12-27 12:35:45 -0800

    From city

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Created on Tue Dec 27 2022
+
+@author: Godday Okoduwa
+"""
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    rows = session.query(State).all()
+    for city in rows:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
