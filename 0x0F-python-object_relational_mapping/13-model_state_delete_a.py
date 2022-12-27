9b5a5e49e9bd125b76143a02c8613046156bf0ba
GoddayOkos <goddayokos@gmail.com>
2022-12-27 05:07:06 -0800

    Delete states containing a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Created on Tue Dec 27 2022
+
+@author: Godday Okoduwa"
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    states = session.query(State).filter(State.name.contains('a'))
+    for state in states:
+        session.delete(state)
+    session.commit()
